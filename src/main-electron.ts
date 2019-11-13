2f5d32eac73e68908bb0684b46e0212d90af15c0
jsbourdon <bourdon.js@gmail.com>
2019-11-13 16:35:53 -0500

    Adding support for custom `module` protocol to allow Electron renderers to load transpiled modules

--- src/main-electron.ts
+++ src/main-electron.ts
@@ -1,12 +1,52 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { app, BrowserWindow, ipcMain, protocol } from 'electron';
+import { Constants } from './models/Constants';
 import url from 'url';
 import path from 'path';
-import { Constants } from './models/Constants';
+import fs from 'fs';
 
 let mainWindow : Electron.BrowserWindow | null;
 
+function registerModuleProtocol(): void
+{
+    // Register custom protocal to allow Electron Renderers
+    // to load javascript modules transpiled from typescript.
+    protocol.registerBufferProtocol("module", (request, callback) =>
+    {
+        const url: string = request.url.substr(9);
+        const filePath: string = path.normalize(`${__dirname}/${url}`);
+        const fileData: Buffer = fs.readFileSync(filePath);
+
+        let reply: any = null;
+
+        const ext: string = path.extname(filePath);
+        if (ext !== '.map')
+        {
+            const header: Buffer = Buffer.from("let exports = {};");
+            const repliedData: Buffer = Buffer.concat([header, fileData], header.length + fileData.length);
+
+            reply =
+                {
+                    data: repliedData,
+                    mimeType: 'text/javascript'
+                };
+        }
+        else
+        {
+            reply =
+                {
+                    data: fileData,
+                    mimeType: 'application/json'
+                };
+        }
+
+        callback(reply);
+    });
+}
+
 app.on('ready', () =>
 {
+    registerModuleProtocol();
+
     const mainWindowPath : string = path.join(__dirname, '../src/views/MainWindow.html');
     let mainWindowUrl : url.URL = new url.URL(mainWindowPath);
     mainWindowUrl.protocol = 'file:';
